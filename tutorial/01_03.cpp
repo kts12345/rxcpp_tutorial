51d0756382929a06d305b14e9dae480e33cb3121
kts123 <kts123@neowiz.com>
2015-09-01 05:11:39 +0900

    typo

--- tutorial/01_03.cpp
+++ tutorial/01_03.cpp
@@ -15,7 +15,7 @@ void test_01_03() {
     std::array<std::function<void()>, 3> events = {
       [=] { observer.on_next(evt_t(31)); }
     , [=] { observer.on_completed();     }
-    , [=] { observer.on_next(evt_t(31)); }
+    , [=] { observer.on_next(evt_t(32)); }
     };
 
     set_timeout(events[0], 1000ms);
@@ -26,7 +26,7 @@ void test_01_03() {
     observer.add(on_dispose);
   };
 
-  auto handler_next      = [](evt_t i) { console::log  ("on_next: "s + i); };
+  auto handler_next      = [](evt_t i) { console::log  ("on_next: "s  + i); };
   auto handler_error     = [](auto  e) { console::error("on_error: "s + e); };
   auto handler_completed = []          { console::log  ("on_completed");    };
 
