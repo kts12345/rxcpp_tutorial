29e7596fa8446f14080a4de04de64d1142dc1a2d
kts123 <kts123@neowiz.com>
2015-09-01 05:09:18 +0900

    clear_timeout

--- node_util/set_timeout.hpp
+++ node_util/set_timeout.hpp
@@ -19,19 +19,44 @@ io_service* default_io_service(io_service* service = nullptr) {
     return service_;
 }
 
+inline 
+std::map<int, std::shared_ptr<system_timer>> &
+g_timers() {
+  static std::map<int, std::shared_ptr<system_timer>> _timers;
+  return _timers;
+}
+
 template<typename callback_t, typename time_t>
-static auto set_timeout(callback_t& callback, 
+static int set_timeout(callback_t& callback, 
                         time_t&     time, 
                         io_service* io = nullptr) {
+
+  static int _id = 0;
+  auto id = _id++;
 
   auto& io_service = *default_io_service(io);
 
   auto timer = std::make_shared<system_timer>(io_service, time);
+  g_timers()[id] = timer;
 
-  timer->async_wait([timer, callback](auto& ec) {
-    callback();
+  timer->async_wait([callback, id](auto& ec) {
+    g_timers()[id].reset();
+    if (!ec)
+      callback();
   });
+
+  return id;
 };
+
+inline void clear_timeout(int id)
+{
+  using namespace std::literals::chrono_literals;
+
+  if (g_timers()[id])
+  {
+    g_timers()[id]->expires_from_now(0ms);
+  }
+}
 
 }// end of namespace node_util
 
